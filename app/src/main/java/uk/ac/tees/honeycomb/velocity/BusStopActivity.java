faf65c1a604da3f2576fa9c0a755868edab888b4
Jordon SEYMOUR <v8027914@linux22.scm.tees.ac.uk>
2020-02-20 13:20:41 +0000

    removed CD, doing table operator now with minimal functionality

--- app/src/main/java/uk/ac/tees/honeycomb/velocity/BusStopActivity.java
+++ app/src/main/java/uk/ac/tees/honeycomb/velocity/BusStopActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import java.util.HashMap;
+import java.util.SplittableRandom;
 
 public class BusStopActivity extends AppCompatActivity {
     //Implement interface BusStopActivity
@@ -35,19 +38,50 @@ public class BusStopActivity extends AppCompatActivity {
 
 
 
-        CardView ct = (CardView) findViewById(R.id.cd);
-        String[] textArray = {"One", "Two", "Three", "Four"};
+        TableLayout tb = (TableLayout) findViewById(R.id.Table);
+        tb.removeAllViews();
+        TableRow[] rows1 = new TableRow[5];
+        TableRow[] rows2 = new TableRow[5];
+      //  TableRow tr = (TableRow) findViewById(R.id.tr1);
 
-        TextView[] t = new TextView[10];
+        TextView test = new TextView(this);
+        test.setText("Time");
 
-for(int i=0;i<10;i++) {
+        String[] textArray1 = {"Tower", "TCollegeroad", "Priston piece", "bustop1","bustop2"};
+        String[] textArray2 = {"10:14", "10:30", "10:44", "10:50","10:58"};
+        TextView[] t = new TextView[5];
+        TextView[] t2 = new TextView[5];
+        TableRow bus = new TableRow(this);
 
-    t[i] = new TextView(this);
-    t[i].setText(message + i);
-    t[i].setBackgroundColor(Color.RED);
-    ct.addView(t[i]);
+        TextView bust = new TextView(this);
+        TextView timet = new TextView(this);
+        timet.setText("Time");
+        bust.setText("BusStop");
+
+
+        bus.addView(timet);
+        bus.addView(bust);
+        bus.setPaddingRelative(200,0,100,400);
+        tb.addView(bus);
+for(int i=0;i<5;i++) {
+rows1[i] = new TableRow(this);
+
+rows2[i] = new TableRow(this);
+
+t[i] = new TextView(this);
+t2[i] = new TextView(this);
+t[i].setText(textArray1[i]);
+    t2[i].setText(textArray2[i]);
+rows1[i].addView(t[i]);
+rows1[i].setPadding(100,10,250,0);
+    rows1[i].addView(t2[i]);
+
+    tb.addView(rows1[i]);
+
+
 
 }
+
 
     }
 
