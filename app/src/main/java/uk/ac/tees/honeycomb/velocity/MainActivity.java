2a7957688a336617a89f551a10dea931e9510bd4
Samuel-Bates <55737338+Samuel-Bates@users.noreply.github.com>
2020-02-21 04:24:17 +0000

    Journey planner skeleton: - Ability to switch directions of a bus ride. - List of possible connections (for now named bus stops).

--- app/src/main/java/uk/ac/tees/honeycomb/velocity/MainActivity.java
+++ app/src/main/java/uk/ac/tees/honeycomb/velocity/MainActivity.java
@@ -6,8 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -15,6 +14,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 
     public void redirectJourney(View view)
     {
