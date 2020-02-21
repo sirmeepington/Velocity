4b9ade03dd008df66bb519d7389bcf08cdc86bdc
Aidan <sir.meepington.1@gmail.com>
2020-02-21 11:06:13 +0000

    Remove unnecessary string allocation and make explicit generic into diamond operator.

--- app/src/main/java/uk/ac/tees/honeycomb/velocity/MainActivity.java
+++ app/src/main/java/uk/ac/tees/honeycomb/velocity/MainActivity.java
@@ -15,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
     public void redirectJourney(View view)
     {
         Intent intent = new Intent(this, JourneyPlannerActivity.class);
