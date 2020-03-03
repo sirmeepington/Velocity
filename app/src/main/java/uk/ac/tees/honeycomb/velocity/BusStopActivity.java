48ff23f419ee9ac6c818909780f4557258191117
Jordon Seymour <v8027914@tees.ac.uk>
2020-03-03 12:30:43 +0000

    Standards changes. -Names -Validation on response. -user friendly changes

--- app/src/main/java/uk/ac/tees/honeycomb/velocity/BusStopActivity.java
+++ app/src/main/java/uk/ac/tees/honeycomb/velocity/BusStopActivity.java
@@ -29,6 +29,8 @@ import uk.ac.tees.honeycomb.velocity.stops.NaptanBusStop;
 
 public class BusStopActivity extends AppCompatActivity {
 
+    ArrayList<String> ATCOCode = new ArrayList<>();
+    ArrayList<String> busStopNames = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,42 +38,55 @@ public class BusStopActivity extends AppCompatActivity {
     }
 
 
-    public void showBusStop(View view)
+    public void showBusStopViaName(View view)
     {
         final Spinner choices = (Spinner) findViewById(R.id.spinner_busstop);
 
 final Object type = this;
-        final EditText editText = (EditText) findViewById(R.id.busStopInput);
-        final String message = editText.getText().toString();
+        final EditText busStopInput = (EditText) findViewById(R.id.busStopInput);
+        final String message = busStopInput.getText().toString();
 
         final StopByName SBN = new StopByName(this,message);
-        editText.setHint("Loading");
-        editText.setText("");
+        busStopInput.setHint("Loading");
+        busStopInput.setText("");
 
         SBN.query(
                 new Response.Listener<ImpetusResponse<StopByNameResponse>>() {
                     @Override
                     public void onResponse(ImpetusResponse<StopByNameResponse> response) {
                         Log.d("Velocity",response.getMessage().getDataSource());
+                        busStopInput.setHint("Loaded Successfully");
                         List<NaptanBusStop> stops = response.getMessage().getData();
                         if (stops == null){
                             Log.d("Velocity","No values found.");
+                            busStopInput.setHint("Error. Stop Name Does Not Exist");
                             return;
                         }
-                        ArrayList<String> arrayList = new ArrayList<>();
+
+                        if(!ATCOCode.isEmpty()) {
+                            ATCOCode.removeAll(ATCOCode);
+                            busStopNames.removeAll(busStopNames);
+                        }
                         for (NaptanBusStop stop : stops) {
                             Log.d("Velocity Result", stop.getName());
 
-                            arrayList.add(stop.getName() + " " + stop.getLocality());
+                            busStopNames.add(stop.getName() + " " + stop.getLocality());
+                           ATCOCode.add(stop.getAtcoCode());
 
                         }
-                        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>((Context) type, android.R.layout.simple_spinner_item, arrayList);
+                        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>((Context) type, android.R.layout.simple_spinner_item, busStopNames);
                         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                         choices.setAdapter(arrayAdapter);
                         choices.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                             @Override
                             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+                                TableLayout tb = (TableLayout) findViewById(R.id.score_table);
+                                tb.removeAllViews();
+                                TableRow tr = new TableRow((Context) type);
+                                TextView test = new TextView((Context) type);
+                                test.setText(ATCOCode.get(position).toString());
+                                tr.addView(test);
+                                tb.addView(tr);
                             }
 
                             @Override
@@ -85,55 +100,15 @@ final Object type = this;
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.e("Velocity", "Error"+error);
-                    }
+                        Log.e("Velocity", "Error"+error); }
+                    
+
                 });
 
+    }
 
-
-//editText.setText(list.get(list.size()).toString());
-
-     /*
-        tb.removeAllViews();
-
-      //  TableRow tr = (TableRow) findViewById(R.id.tr1);
-
-        TextView test = new TextView(this);
-        test.setText("Time");
-
-        String[] textArray1 = {"8", "5A", "10", "14","62A"};
-        String[] textArray2 = {"10:14", "10:30", "10:44", "10:50","10:58"};
-        String[] textArray3 = {"Arriva","StageCoach" ,"Arriva","megaBus","National Holidays"};
-
-        TextView[] t3 = new TextView[5];
-        TableRow bus = new TableRow(this);
-
-
-      //  bus.setPaddingRelative(200,0,100,400);
-        tb.addView(bus);
-        for(int j = 0;j<6;j++) {
-            for (int i = 0; i < 5; i++) {
-                rows1[i] = new TableRow(this);
-
-                rows2[i] = new TableRow(this);
-
-                t[i] = new TextView(this);
-                t2[i] = new TextView(this);
-                t3[i] = new TextView(this);
-                t[i].setText(textArray1[i]);
-                t2[i].setText(textArray2[i]);
-                t3[i].setText(textArray3[i]);
-               t2[i].setPadding(290, 0, 0, 0);
-                t3[i].setPadding(60, 0, 0, 0);
-                rows1[i].addView(t[i]);
-                // rows1[i].setPadding(100, 10, 250, 0);
-                rows1[i].addView(t2[i]);
-                rows1[i].addView(t3[i]);
-                tb.addView(rows1[i]);
-
-
-            }
-        }*/
+    public void getTimesViaATCO()
+    {
 
     }
 }
