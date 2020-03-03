e1d6caa0dac38ad53bfdf1e106f7c98d174aea53
Aidan <sir.meepington.1@gmail.com>
2020-03-03 16:08:40 +0000

    [#171212375] Begin to implement the Bus-Stop Timetable (#8)
    * Better Table and double constraints added along with spinner
    
    * Table names changed and moved.
    
    * Adding to spinner works
    
    * Standards changes.
    -Names
    -Validation on response.
    -user friendly changes
    
    * Use HashMap for stops instead of two ArrayLists
    
    Co-authored-by: Jordmore <55835147+Jordmore@users.noreply.github.com>

--- app/src/main/java/uk/ac/tees/honeycomb/velocity/BusStopActivity.java
+++ app/src/main/java/uk/ac/tees/honeycomb/velocity/BusStopActivity.java
@@ -1,15 +1,38 @@
 package uk.ac.tees.honeycomb.velocity;
 
+import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import uk.ac.tees.honeycomb.velocity.api.entities.endpoints.StopByName;
+import uk.ac.tees.honeycomb.velocity.api.entities.responses.ImpetusResponse;
+import uk.ac.tees.honeycomb.velocity.api.entities.responses.StopByNameResponse;
+import uk.ac.tees.honeycomb.velocity.stops.NaptanBusStop;
+
 public class BusStopActivity extends AppCompatActivity {
+
+
+    HashMap<String,String> loadedStops = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,59 +40,87 @@ public class BusStopActivity extends AppCompatActivity {
         setContentView(R.layout.busstop_activity);
     }
 
-    private void addingToCD(){
+
+    public void showBusStopViaName(View view)
+    {
+        final Spinner choices = (Spinner) findViewById(R.id.spinner_busstop);
+
+        final Context type = this;
+        final EditText busStopInput = (EditText) findViewById(R.id.busStopInput);
+        final String message = busStopInput.getText().toString();
+
+        final StopByName SBN = new StopByName(this,message);
+        busStopInput.setHint("Loading");
+        busStopInput.setText("");
+
+        SBN.query(
+                new Response.Listener<ImpetusResponse<StopByNameResponse>>() {
+                    @Override
+                    public void onResponse(ImpetusResponse<StopByNameResponse> response) {
+                        Log.d("Velocity",response.getMessage().getDataSource());
+                        busStopInput.setHint("Loaded Successfully");
+                        final List<NaptanBusStop> stops = response.getMessage().getData();
+                        if (stops == null){
+                            Log.d("Velocity","No values found.");
+                            busStopInput.setHint("Error. Stop Name Does Not Exist");
+                            return;
+                        }
+
+                        if(!loadedStops.isEmpty()) {
+                            loadedStops.clear();
+                        }
+                        for (NaptanBusStop stop : stops) {
+                            Log.d("Velocity Result", stop.getName());
+
+                            loadedStops.put(stop.getAtcoCode().toLowerCase(),stop.getName() + " - " + stop.getLocality());
+                        }
+
+                        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(type, android.R.layout.simple_spinner_item, new ArrayList<>(loadedStops.values()));
+                        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        choices.setAdapter(arrayAdapter);
+                        choices.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                            @Override
+                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                TableLayout tb = (TableLayout) findViewById(R.id.score_table);
+                                tb.removeAllViews();
+                                TableRow tr = new TableRow(type);
+                                TextView test = new TextView(type);
+                                test.setText(getStopAtPos(position).getKey());
+                                tr.addView(test);
+                                tb.addView(tr);
+                            }
+
+                            @Override
+                            public void onNothingSelected(AdapterView<?> parent) {
+
+                            }
+                        });
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Velocity", "Error"+error); }
+                    
+
+                });
 
     }
 
-    public void showBusStop(View view)
-    {
-        EditText editText = (EditText) findViewById(R.id.busStopInput);
-        String message = editText.getText().toString();
-
-
-
-        TableLayout tb = (TableLayout) findViewById(R.id.Table);
-        tb.removeAllViews();
-        TableRow[] rows1 = new TableRow[5];
-        TableRow[] rows2 = new TableRow[5];
-      //  TableRow tr = (TableRow) findViewById(R.id.tr1);
-
-        TextView test = new TextView(this);
-        test.setText("Time");
-
-        String[] textArray1 = {"Tower", "TCollegeroad", "Priston piece", "bustop1","bustop2"};
-        String[] textArray2 = {"10:14", "10:30", "10:44", "10:50","10:58"};
-        TextView[] t = new TextView[5];
-        TextView[] t2 = new TextView[5];
-        TableRow bus = new TableRow(this);
-
-        TextView bust = new TextView(this);
-        TextView timet = new TextView(this);
-        timet.setText("Time");
-        bust.setText("BusStop");
-
-
-        bus.addView(timet);
-        bus.addView(bust);
-        bus.setPaddingRelative(200,0,100,400);
-        tb.addView(bus);
-        for (int i = 0; i < 5; i++) {
-            rows1[i] = new TableRow(this);
-
-            rows2[i] = new TableRow(this);
-
-            t[i] = new TextView(this);
-            t2[i] = new TextView(this);
-            t[i].setText(textArray1[i]);
-            t2[i].setText(textArray2[i]);
-            rows1[i].addView(t[i]);
-            rows1[i].setPadding(100, 10, 250, 0);
-            rows1[i].addView(t2[i]);
-
-            tb.addView(rows1[i]);
-
-
+    private Map.Entry<String,String> getStopAtPos(int pos){
+        int i = 0;
+        for(Map.Entry<String,String> entry : loadedStops.entrySet()){
+            if (i == pos){
+                return entry;
+            }
+            i++;
         }
+        return null;
+    }
+
+    public void getTimesViaATCO()
+    {
 
     }
 }
