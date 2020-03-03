e55a04ccc6944b1dff3a57d0ee98a2bd63c7428b
Sebastian <sebastian.s.cieslik@gmail.com>
2020-03-03 16:20:37 +0000

    Validation, automatic selection

--- app/src/main/java/uk/ac/tees/honeycomb/velocity/JourneyPlannerActivity.java
+++ app/src/main/java/uk/ac/tees/honeycomb/velocity/JourneyPlannerActivity.java
@@ -1,16 +1,23 @@
 package uk.ac.tees.honeycomb.velocity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.sql.Time;
+
 public class JourneyPlannerActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,23 +27,29 @@ public class JourneyPlannerActivity extends AppCompatActivity {
 
     public void jpConfirm(View view){
 
+        if(checkFromText() == false) {
+            final ListView journey = (ListView) findViewById(R.id.busStops);
 
-        final ListView journey = (ListView)findViewById(R.id.busStops);
+            String[] busStops = {"BusStop1", "busStop2"};
+            Time[] busStopTimes = {};
+            ArrayAdapter<String> bsAdp = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, busStops);
+            journey.setAdapter(bsAdp);
+            journey.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    int serviceNo = position;
+                    String busStopName = (String) journey.getItemAtPosition(position);
 
-        String[] busStops = {"BusStop1", "busStop2"};
-
-        ArrayAdapter<String> bsAdp = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,android.R.id.text1,busStops);
-        journey.setAdapter(bsAdp);
-        journey.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                int serviceNo = position;
-                String busStopName = (String)journey.getItemAtPosition(position);
-
-                Toast.makeText(getApplicationContext(), serviceNo + " "  + busStopName, Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), serviceNo + " " + busStopName, Toast.LENGTH_LONG).show();
+                }
+            });
+        }else{
+            AlertDialog.Builder errEmptyFrom = new AlertDialog.Builder(this);
+            errEmptyFrom.setMessage("Error: Please make sure the 'From' field contains text.");
+            errEmptyFrom.show();
             }
-        });
-    }
+        }
+
 
     public void jpSwitch(View view) {
         EditText from = (EditText) findViewById(R.id.jpFrom);
@@ -47,4 +60,15 @@ public class JourneyPlannerActivity extends AppCompatActivity {
         from.setText(strTo);
         to.setText(strFrom);
     }
+    public boolean checkFromText(){
+        EditText from = (EditText) findViewById(R.id.jpFrom);
+        String fromText = from.getText().toString();
+
+        if(fromText.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
