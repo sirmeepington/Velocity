25d107db20de81238ab61d93e714db90f6bb6965
Jordon Seymour <v8027914@tees.ac.uk>
2020-03-05 14:04:12 +0000

    Added Arraylist ATCO as i didnt know how to get the Hashmap key for the Time table Added class getTimesViaATCO.

--- app/src/main/java/uk/ac/tees/honeycomb/velocity/BusStopActivity.java
+++ app/src/main/java/uk/ac/tees/honeycomb/velocity/BusStopActivity.java
@@ -20,6 +20,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -28,14 +29,19 @@ import java.util.regex.Pattern;
 
 import uk.ac.tees.honeycomb.velocity.api.entities.endpoints.StopByName;
 import uk.ac.tees.honeycomb.velocity.api.entities.endpoints.StopByPostcode;
+import uk.ac.tees.honeycomb.velocity.api.entities.endpoints.StopFromAtcoCode;
+import uk.ac.tees.honeycomb.velocity.api.entities.endpoints.StopTimetable;
+import uk.ac.tees.honeycomb.velocity.api.entities.responses.BusStopTimetableResponse;
 import uk.ac.tees.honeycomb.velocity.api.entities.responses.ImpetusResponse;
 import uk.ac.tees.honeycomb.velocity.api.entities.responses.StopByNameResponse;
 import uk.ac.tees.honeycomb.velocity.api.entities.responses.StopByPostcodeResponse;
+import uk.ac.tees.honeycomb.velocity.api.entities.responses.StopFromAtcoCodeResponse;
 import uk.ac.tees.honeycomb.velocity.stops.NaptanBusStop;
 
 public class BusStopActivity extends AppCompatActivity {
-
-    HashMap<String,String> loadedStops = new HashMap<>();
+    final Context type = this;
+    HashMap<String, String> loadedStops = new HashMap<>();
+    ArrayList<String> atco = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,61 +49,54 @@ public class BusStopActivity extends AppCompatActivity {
         setContentView(R.layout.busstop_activity);
     }
 
-public void redirectButton(View view)
-{
+    public void redirectButton(View view) {
 
-    final EditText busStopInput = (EditText) findViewById(R.id.busStopInput);
+        final EditText busStopInput = findViewById(R.id.busStopInput);
 
 
-    if(validatePostCode(busStopInput.getText().toString()))
-    {
-        showBusStopsViaPostCode(view);
+        if (validatePostCode(busStopInput.getText().toString())) {
+            showBusStopsViaPostCode(view);
+
+        } else {
+            showBusStopViaName(view);
+        }
+    }
+
+
+    public boolean validatePostCode(String postCode) {
+        Pattern pattern = Pattern.compile("([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\\s?[0-9][A-Za-z]{2})");
+        return pattern.matcher(postCode).matches();
 
     }
-    else
-    {
-        showBusStopViaName(view);
-    }
-}
 
+    public void showBusStopsViaPostCode(View view) {
+        final Spinner choices = findViewById(R.id.spinner_busstop);
 
-
-
-
-public boolean validatePostCode(String postCode)
-{
-    Pattern pattern = Pattern.compile("([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\\s?[0-9][A-Za-z]{2})");
-   return pattern.matcher(postCode).matches();
-
-}
-public void showBusStopsViaPostCode(View view)
-    {
-        final Spinner choices = (Spinner) findViewById(R.id.spinner_busstop);
-        final Context type = this;
-        final EditText busStopInput = (EditText) findViewById(R.id.busStopInput);
+        final EditText busStopInput = findViewById(R.id.busStopInput);
         final String message = busStopInput.getText().toString();
 
-        final StopByPostcode SBP = new StopByPostcode(this,message);
+        final StopByPostcode SBP = new StopByPostcode(this, message);
         SBP.query(
                 new Response.Listener<ImpetusResponse<StopByPostcodeResponse>>() {
                     @Override
                     public void onResponse(ImpetusResponse<StopByPostcodeResponse> response) {
-                        Log.d("Velocity",response.getMessage().getSources().toString());
+                        Log.d("Velocity", response.getMessage().getSources().toString());
                         busStopInput.setHint("Loaded Successfully");
                         final List<NaptanBusStop> stops = response.getMessage().getData();
-                        if (stops == null){
-                            Log.d("Velocity","No values found.");
+                        if (stops == null) {
+                            Log.d("Velocity", "No values found.");
                             busStopInput.setHint("Error. Stop Name Does Not Exist");
                             return;
                         }
 
-                        if(!loadedStops.isEmpty()) {
+                        if (!loadedStops.isEmpty()) {
                             loadedStops.clear();
                         }
                         for (NaptanBusStop stop : stops) {
                             Log.d("Velocity Result", stop.getName());
 
-                            loadedStops.put(stop.getAtcoCode().toLowerCase(),stop.getName() + " - " + stop.getLocality());
+                            loadedStops.put(stop.getAtcoCode().toLowerCase(), stop.getName() + " - " + stop.getLocality());
+                            atco.add(stop.getAtcoCode().toLowerCase());
                         }
 
                         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(type, android.R.layout.simple_spinner_item, new ArrayList<>(loadedStops.values()));
@@ -106,13 +105,7 @@ public void showBusStopsViaPostCode(View view)
                         choices.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                             @Override
                             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                                TableLayout tb = (TableLayout) findViewById(R.id.score_table);
-                                tb.removeAllViews();
-                                TableRow tr = new TableRow(type);
-                                TextView test = new TextView(type);
-                                test.setText(getStopAtPos(position).getKey());
-                                tr.addView(test);
-                                tb.addView(tr);
+                                getTimesViaATCO(atco.get(position));
                             }
 
                             @Override
@@ -126,42 +119,44 @@ public void showBusStopsViaPostCode(View view)
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.e("Velocity", "Error"+error); }
+                        Log.e("Velocity", "Error" + error);
+                    }
 
 
                 });
     }
-    public void showBusStopViaName(View view)
-    {
-        final Spinner choices = (Spinner) findViewById(R.id.spinner_busstop);
-        final Context type = this;
-        final EditText busStopInput = (EditText) findViewById(R.id.busStopInput);
+
+    public void showBusStopViaName(View view) {
+        final Spinner choices = findViewById(R.id.spinner_busstop);
+
+        final EditText busStopInput = findViewById(R.id.busStopInput);
         final String message = busStopInput.getText().toString();
 
-        final StopByName SBN = new StopByName(this,message);
+        final StopByName SBN = new StopByName(this, message);
         busStopInput.setHint("Loading");
         busStopInput.setText("");
 
         SBN.query(
                 new Response.Listener<ImpetusResponse<StopByNameResponse>>() {
                     @Override
-                    public void onResponse(ImpetusResponse<StopByNameResponse> response) {
-                        Log.d("Velocity",response.getMessage().getDataSource());
+                    public void onResponse(final ImpetusResponse<StopByNameResponse> response) {
+                        Log.d("Velocity", response.getMessage().getDataSource());
                         busStopInput.setHint("Loaded Successfully");
                         final List<NaptanBusStop> stops = response.getMessage().getData();
-                        if (stops == null){
-                            Log.d("Velocity","No values found.");
+                        if (stops == null) {
+                            Log.d("Velocity", "No values found.");
                             busStopInput.setHint("Error. Stop Name Does Not Exist");
                             return;
                         }
 
-                        if(!loadedStops.isEmpty()) {
+                        if (!loadedStops.isEmpty()) {
                             loadedStops.clear();
                         }
                         for (NaptanBusStop stop : stops) {
                             Log.d("Velocity Result", stop.getName());
 
-                            loadedStops.put(stop.getAtcoCode().toLowerCase(),stop.getName() + " - " + stop.getLocality());
+                            loadedStops.put(stop.getAtcoCode().toLowerCase(), stop.getName() + " - " + stop.getLocality());
+                            atco.add(stop.getAtcoCode().toLowerCase());
                         }
 
                         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(type, android.R.layout.simple_spinner_item, new ArrayList<>(loadedStops.values()));
@@ -170,13 +165,7 @@ public void showBusStopsViaPostCode(View view)
                         choices.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                             @Override
                             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                                TableLayout tb = (TableLayout) findViewById(R.id.score_table);
-                                tb.removeAllViews();
-                                TableRow tr = new TableRow(type);
-                                TextView test = new TextView(type);
-                                test.setText(getStopAtPos(position).getKey());
-                                tr.addView(test);
-                                tb.addView(tr);
+                                getTimesViaATCO(atco.get(position));
                             }
 
                             @Override
@@ -190,17 +179,18 @@ public void showBusStopsViaPostCode(View view)
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.e("Velocity", "Error"+error); }
-                    
+                        Log.e("Velocity", "Error" + error);
+                    }
+
 
                 });
 
     }
 
-    private Map.Entry<String,String> getStopAtPos(int pos){
+    private Map.Entry<String, String> getStopAtPos(int pos) {
         int i = 0;
-        for(Map.Entry<String,String> entry : loadedStops.entrySet()){
-            if (i == pos){
+        for (Map.Entry<String, String> entry : loadedStops.entrySet()) {
+            if (i == pos) {
                 return entry;
             }
             i++;
@@ -208,8 +198,34 @@ public void showBusStopsViaPostCode(View view)
         return null;
     }
 
-    public void getTimesViaATCO()
-    {
+    public void getTimesViaATCO(String atco) {
 
+        final StopTimetable StopTimeTable = new StopTimetable(this, atco);
+
+        StopTimeTable.query(
+                new Response.Listener<ImpetusResponse<BusStopTimetableResponse>>() {
+                    @Override
+                    public void onResponse(final ImpetusResponse<BusStopTimetableResponse> response) {
+                        Log.d("Velocity", response.getMessage().getStopTimetable().toString());
+
+
+                        TableLayout tb = findViewById(R.id.score_table);
+                        tb.removeAllViews();
+                        TableRow tr = new TableRow(type);
+                        TextView test = new TextView(type);
+                        test.setText((CharSequence) response.getMessage().getStopTimetable().getDepartures().get(0));
+                        tr.addView(test);
+                        tb.addView(tr);
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Velocity", "Error" + error);
+                    }
+
+
+                });
     }
 }
