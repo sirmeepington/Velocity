47a33fef89b73259a177ac739ada74bcf00b5e2b
Aidan <sir.meepington.1@gmail.com>
2020-03-10 15:52:59 +0000

    Begin switching between fragments on menu button.

--- app/src/main/java/uk/ac/tees/honeycomb/velocity/MainActivity.java
+++ app/src/main/java/uk/ac/tees/honeycomb/velocity/MainActivity.java
@@ -2,6 +2,9 @@ package uk.ac.tees.honeycomb.velocity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.content.Intent;
 import android.net.Uri;
