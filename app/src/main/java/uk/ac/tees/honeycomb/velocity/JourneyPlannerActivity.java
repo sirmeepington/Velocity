185e3b1f35d72d7c79f5c167e10524e7d0363a95
Aidan <sir.meepington.1@gmail.com>
2020-03-10 14:10:22 +0000

    Fix unused resource id.

--- app/src/main/java/uk/ac/tees/honeycomb/velocity/JourneyPlannerActivity.java
+++ app/src/main/java/uk/ac/tees/honeycomb/velocity/JourneyPlannerActivity.java
@@ -15,7 +15,7 @@ public class JourneyPlannerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_journey_planner);
+        //setContentView(R.layout.activity_journey_planner);
     }
 
     public void jpConfirm(View view){
