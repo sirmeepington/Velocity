6226dda7c7609de40a8886e6f4a671865894db80
Aidan Crooks <sir.meepington.1@gmail.com>
2020-03-13 06:50:34 +0000

    Optimizations, use string resource, fix longlat format.

--- app/src/main/java/uk/ac/tees/honeycomb/velocity/JourneyPlannerActivity.java
+++ app/src/main/java/uk/ac/tees/honeycomb/velocity/JourneyPlannerActivity.java
@@ -1,31 +1,22 @@
 package uk.ac.tees.honeycomb.velocity;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.google.android.gms.common.util.ArrayUtils;
 
-
-import java.sql.Array;
-import java.sql.Time;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Locale;
 
 import uk.ac.tees.honeycomb.velocity.api.entities.endpoints.StopByName;
 import uk.ac.tees.honeycomb.velocity.api.entities.responses.ImpetusResponse;
@@ -39,6 +30,7 @@ public class JourneyPlannerActivity extends AppCompatActivity {
     ArrayList<String> fromListLonglat = new ArrayList<>();
     ArrayList<String> toList = new ArrayList<>();
     ArrayList<String> toListLongLat = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,17 +38,17 @@ public class JourneyPlannerActivity extends AppCompatActivity {
     }
 
     public void jpConfirm(View view){
-        final EditText fromInput = (EditText) findViewById(R.id.jpFrom);
+        final EditText fromInput = findViewById(R.id.jpFrom);
         String fromInputText = fromInput.getText().toString();
 
-        final EditText toInput = (EditText) findViewById(R.id.jpTo);
+        final EditText toInput = findViewById(R.id.jpTo);
         String toInputText =  toInput.getText().toString();
 
         final StopByName fromStop = new StopByName(this, fromInputText);
         final StopByName toStop = new StopByName(this, toInputText);
 
-        final ListView fromDisplayedBusList = (ListView) findViewById(R.id.jpListView);
-        final ListView toDisplayedBusList = (ListView) findViewById(R.id.jpListView);
+        final ListView fromDisplayedBusList = findViewById(R.id.jpListView);
+        final ListView toDisplayedBusList = findViewById(R.id.jpListView);
 
         if (checkFromText() || checkToText()){
             AlertDialog.Builder errEmptyFrom = new AlertDialog.Builder(this);
@@ -67,10 +59,7 @@ public class JourneyPlannerActivity extends AppCompatActivity {
             InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
 
-
-
             fromStop.query(
-
                     new Response.Listener<ImpetusResponse<StopByNameResponse>>() {
                         @Override
                         public void onResponse(ImpetusResponse<StopByNameResponse> response) {
@@ -107,79 +96,65 @@ public class JourneyPlannerActivity extends AppCompatActivity {
                         }
                     }
             );
-            toStop.query(new Response.Listener<ImpetusResponse<StopByNameResponse>>() {
-                @Override
-                public void onResponse(ImpetusResponse<StopByNameResponse> response) {
-                    ArrayList<NaptanBusStop> toBusList = (ArrayList<NaptanBusStop>) response.getMessage().getData();
+            toStop.query(
+                new Response.Listener<ImpetusResponse<StopByNameResponse>>() {
+                    @Override
+                    public void onResponse(ImpetusResponse<StopByNameResponse> response) {
+                        ArrayList<NaptanBusStop> toBusList = (ArrayList<NaptanBusStop>) response.getMessage().getData();
 
-                    for (NaptanBusStop s: toBusList){
-                        toList.add(s.getName());
-                        toListLongLat.add (makeLonLat(s.getLocation()));
-                    }
-                    final ArrayAdapter<String> toBusAdapter = new ArrayAdapter<>(JourneyPlannerActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1,toList);
-                    toDisplayedBusList.setAdapter(toBusAdapter);
-                    toDisplayedBusList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                        @Override
-                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                            String toLonglatSeleted = toListLongLat.get(position);
-                            //String busStopName = (String) toDisplayedBusList.getItemAtPosition(position);
-                            Toast.makeText(getApplicationContext(), toLonglatSeleted, Toast.LENGTH_LONG).show();
-                            toInput.setText(toList.get(position));
-                            toBusAdapter.clear();
+                        for (NaptanBusStop s: toBusList){
+                            toList.add(s.getName());
+                            toListLongLat.add (makeLonLat(s.getLocation()));
                         }
-                    });
-                }
-            },
+                        final ArrayAdapter<String> toBusAdapter = new ArrayAdapter<>(JourneyPlannerActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1,toList);
+                        toDisplayedBusList.setAdapter(toBusAdapter);
+                        toDisplayedBusList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                String toLonglatSeleted = toListLongLat.get(position);
+                                //String busStopName = (String) toDisplayedBusList.getItemAtPosition(position);
+                                Toast.makeText(getApplicationContext(), toLonglatSeleted, Toast.LENGTH_LONG).show();
+                                toInput.setText(toList.get(position));
+                                toBusAdapter.clear();
+                            }
+                        });
+                    }
+                },
                 new Response.ErrorListener(){
-                @Override
-                    public void onErrorResponse(VolleyError error){
+                    @Override
+                        public void onErrorResponse(VolleyError error){
 
-                }
+                    }
                 }
             );
-
-
-
-
-
-
-
-
         }
     }
 
     private String makeLonLat(Location location){
-        return String.format("lonlat:{0},{1}", location.getLongitude(), location.getLatitude());
+        return String.format(Locale.ENGLISH,"lonlat:%f,%f", location.getLongitude(), location.getLatitude());
     }
 
     public void jpSwitch (View view) {
-        EditText from = (EditText) findViewById(R.id.jpFrom);
+        EditText from = findViewById(R.id.jpFrom);
         String strFrom = from.getText().toString();
-        EditText to = (EditText) findViewById(R.id.jpTo);
+        EditText to = findViewById(R.id.jpTo);
         String strTo = to.getText().toString();
 
         from.setText(strTo);
         to.setText(strFrom);
     }
-    public boolean checkFromText(){
-        EditText from = (EditText) findViewById(R.id.jpFrom);
-        String fromText = from.getText().toString();
 
-        if(fromText.isEmpty()){
-            return true;
-        }else{
-            return false;
-        }
+    private String getEditTextContents(int id){
+        EditText txt = findViewById(id);
+        return txt.getText().toString();
     }
-    public boolean checkToText(){
-        EditText to = (EditText) findViewById(R.id.jpTo);
-        String toText = to.getText().toString();
 
-        if(toText.isEmpty()){
-            return true;
-        }else{
-            return false;
-        }
+    public boolean checkFromText(){
+        return getEditTextContents(R.id.jpFrom).isEmpty();
+    }
+
+    public boolean checkToText(){
+        return getEditTextContents(R.id.jpTo).isEmpty();
     }
 
 
