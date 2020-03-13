7cddd765636230b3d4a817bd09040581731c3b90
Aidan Crooks <sir.meepington.1@gmail.com>
2020-03-13 06:38:04 +0000

    Revert changes to main activity.

--- app/src/main/java/uk/ac/tees/honeycomb/velocity/MainActivity.java
+++ app/src/main/java/uk/ac/tees/honeycomb/velocity/MainActivity.java
@@ -6,18 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_journey_planner);
+        setContentView(R.layout.activity_main);
     }
 
     public void redirectJourney(View view)
     {
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, JourneyPlannerActivity.class);
         startActivity(intent);
     }
 
