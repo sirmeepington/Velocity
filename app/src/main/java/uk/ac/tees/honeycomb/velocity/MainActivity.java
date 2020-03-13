ddd6100fb6389f65a245197757910ed126b590fe
Aidan Crooks <sir.meepington.1@gmail.com>
2020-03-13 08:14:44 +0000

    Use FragmentTransaction to navigate through fragments.

--- app/src/main/java/uk/ac/tees/honeycomb/velocity/MainActivity.java
+++ app/src/main/java/uk/ac/tees/honeycomb/velocity/MainActivity.java
@@ -1,26 +1,31 @@
 package uk.ac.tees.honeycomb.velocity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.NavDirections;
-import androidx.navigation.NavGraph;
-import androidx.navigation.Navigation;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import uk.ac.tees.honeycomb.velocity.fragments.JourneyPlannerFragment;
 import uk.ac.tees.honeycomb.velocity.fragments.MainFragment;
-import uk.ac.tees.honeycomb.velocity.fragments.MainFragmentDirections;
+import uk.ac.tees.honeycomb.velocity.fragments.MapsFragment;
 import uk.ac.tees.honeycomb.velocity.fragments.StopTimetableFragment;
 
-public class MainActivity extends FragmentActivity implements MainFragment.OnFragmentInteractionListener, StopTimetableFragment.OnFragmentInteractionListener
+public class MainActivity extends FragmentActivity implements
+        MainFragment.OnFragmentInteractionListener,
+        StopTimetableFragment.OnFragmentInteractionListener,
+        JourneyPlannerFragment.OnFragmentInteractionListener,
+        MapsFragment.OnFragmentInteractionListener
 {
 
     @Override
@@ -31,25 +36,33 @@ public class MainActivity extends FragmentActivity implements MainFragment.OnFra
 
         NavController controller = Navigation.findNavController(this,R.id.nav_host_fragment);
         BottomNavigationView menu = findViewById(R.id.bottom_nav);
+        NavigationUI.setupWithNavController(menu,controller);
+        load(new MainFragment()); // Initial background fragment
 
         menu.setOnNavigationItemSelectedListener(item -> {
-            if (controller.getCurrentDestination().getId() != R.id.fragment_main){
-                controller.navigate(R.id.fragment_main); // Go back to main for compatibility
-            }
-            NavDirections dir = null;
-            switch (item.getItemId()){
-                case R.id.nav_home:
-                    break; // Already gone back to main
+            switch(item.getItemId()){
                 case R.id.nav_bus_stop:
-                    dir = MainFragmentDirections.mainToStopTimetable();
+                    load(new StopTimetableFragment());
+                    return true;
+                case R.id.nav_journey:
+                    load(new JourneyPlannerFragment());
+                    return true;
+                case R.id.nav_home:
+                    load(new MainFragment());
+                    return true;
+                case R.id.nav_map:
+                    load(new MapsFragment());
                 default:
-                    break;
+                    return false;
             }
-            if (dir != null){
-                controller.navigate(dir); // Go to the selected area.
-            }
-            return true;
         });
+    }
+
+    private void load(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container,fragment);
+        transaction.disallowAddToBackStack();
+        transaction.commit();
     }
 
     public void redirectJourney(View view)
@@ -79,4 +92,5 @@ public class MainActivity extends FragmentActivity implements MainFragment.OnFra
     @Override
     public void onFragmentInteraction(Uri uri) {
     }
+
 }
