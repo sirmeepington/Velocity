bc0ddc6ff53532d396b5ff01772b1788a9fbd11f
SEBASTIAN CIESLIK <V8036046@live.tees.ac.uk>
2020-05-01 12:34:18 +0100

    Search buttons added. Response Added.

--- app/src/main/java/uk/ac/tees/honeycomb/velocity/behaviours/JourneyPlanner.java
+++ app/src/main/java/uk/ac/tees/honeycomb/velocity/behaviours/JourneyPlanner.java
@@ -1,7 +1,10 @@
 package uk.ac.tees.honeycomb.velocity.behaviours;
 
 import android.content.Context;
+import android.renderscript.ScriptGroup;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,26 +13,51 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import uk.ac.tees.honeycomb.velocity.R;
+import uk.ac.tees.honeycomb.velocity.api.entities.endpoints.JourneyFromStops;
 import uk.ac.tees.honeycomb.velocity.api.entities.endpoints.StopByName;
+import uk.ac.tees.honeycomb.velocity.api.entities.responses.JourneyResponse;
+import uk.ac.tees.honeycomb.velocity.api.entities.transportapi.Journey;
+import uk.ac.tees.honeycomb.velocity.api.entities.transportapi.JourneyRoute;
 import uk.ac.tees.honeycomb.velocity.entities.Location;
+import uk.ac.tees.honeycomb.velocity.stops.BusStop;
 import uk.ac.tees.honeycomb.velocity.stops.NaptanBusStop;
 
 
 public class JourneyPlanner implements Behaviour {
 
-    private final View parentView;
-    private HashMap<String,String> from = new HashMap<>();
-    private HashMap<String,String> to = new HashMap<>();
+private final View parentView;
+BusStop busStopFrom;
+BusStop busStopTo;
+public boolean fromSearchCheck = false;
+public boolean toSearchCheck = false;
 
     public JourneyPlanner(View parentView){
         this.parentView = parentView;
         createListeners(parentView);
+        showKeyboard();
+    }
+
+    private void showKeyboard(){
+        EditText fromText = (EditText) parentView.findViewById(R.id.jpFrom);
+        fromText.requestFocus();
+        InputMethodManager imm = (InputMethodManager)getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+
+    }
+
+    private void hideKeyboard(){
+        InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(parentView.getWindowToken(),0);
     }
 
     private void createListeners(View view){
@@ -38,6 +66,13 @@ public class JourneyPlanner implements Behaviour {
 
         Button confirmButton = view.findViewById(R.id.jpConfirm);
         confirmButton.setOnClickListener(v -> jpConfirm());
+
+        Button fromSearchButton = view.findViewById(R.id.jpFromSearch);
+        fromSearchButton.setOnClickListener(v -> fromSearch());
+
+        Button toSearchButton = view.findViewById(R.id.jpToSearch);
+        toSearchButton.setOnClickListener(v -> toSearch());
+
 
     }
 
@@ -53,68 +88,41 @@ public class JourneyPlanner implements Behaviour {
     }
 
     public void jpConfirm(){
-        final EditText fromInput = parentView.findViewById(R.id.jpFrom);
-        String fromInputText = fromInput.getText().toString();
+        hideKeyboard();
 
-        final EditText toInput = parentView.findViewById(R.id.jpTo);
-        String toInputText =  toInput.getText().toString();
 
-        final StopByName fromStop = new StopByName(getContext(), fromInputText);
-        final StopByName toStop = new StopByName(getContext(), toInputText);
 
-        final ListView fromDisplayedBusList = parentView.findViewById(R.id.jpListView);
-        final ListView toDisplayedBusList = parentView.findViewById(R.id.jpListView);
+
 
         if (checkFromText() || checkToText()){
             AlertDialog.Builder errEmptyFrom = new AlertDialog.Builder(getContext());
             errEmptyFrom.setMessage("Error: Please make sure the 'From' and 'To' fields are filled.");
             errEmptyFrom.show();
+        }else if (!fromSearchCheck || !toSearchCheck){
+            AlertDialog.Builder errNoSearch = new AlertDialog.Builder(getContext());
+            errNoSearch.setMessage("Please search and select a bus stop.");
+            errNoSearch.show();
+
         }else{
-            fromInput.requestFocus();
+            JourneyFromStops jfs = new JourneyFromStops(getContext(),busStopFrom,busStopTo);
 
-            fromStop.query(
-                    response -> {
-                        ArrayList<NaptanBusStop> fromBusList = (ArrayList<NaptanBusStop>) response.getMessage().getData();
-
-
-                        for(NaptanBusStop s : fromBusList){
-                            from.put(s.getName(),s.getLocation().getLongLat());
-                        }
-                        final ArrayAdapter<String> fromBusAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, android.R.id.text1, new ArrayList<String>(from.keySet()));
-                        fromDisplayedBusList.setAdapter(fromBusAdapter);
-                        fromDisplayedBusList.setOnItemClickListener((parent, view12, position, id) -> {
-                            //saving long and lat of selected bus stop.
-                            String fromLonglatSelected = getCollectionAtIndex(from.values(),position);
-
-                            //String fromBusStopName = (String) fromDisplayedBusList.getItemAtPosition(position);
-                            Toast.makeText(getContext().getApplicationContext(), fromLonglatSelected, Toast.LENGTH_LONG).show();
-                            fromInput.setText(getCollectionAtIndex(from.keySet(),position));
-                            fromBusAdapter.clear();
-                        });
+            jfs.query(response -> {
+                Journey jpResult = (Journey) response.getMessage().getJourney();
 
 
-                    },
-                    error -> { }
-            );
-            toStop.query(
-                    response -> {
-                        ArrayList<NaptanBusStop> toBusList = (ArrayList<NaptanBusStop>) response.getMessage().getData();
 
-                        for (NaptanBusStop s: toBusList){
-                            to.put(s.getName(),s.getLocation().getLongLat());
-                        }
-                        final ArrayAdapter<String> toBusAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, android.R.id.text1,new ArrayList<String>(to.keySet()));
-                        toDisplayedBusList.setAdapter(toBusAdapter);
-                        toDisplayedBusList.setOnItemClickListener((parent, view1, position, id) -> {
-                            String toLonglatSeleted = getCollectionAtIndex(to.values(),position);
-                            Toast.makeText(getContext().getApplicationContext(), toLonglatSeleted, Toast.LENGTH_LONG).show();
-                            toInput.setText(getCollectionAtIndex(to.keySet(),position));
-                            toBusAdapter.clear();
-                        });
-                    },
-                    error -> { }
-            );
+                final ListView resultList = parentView.findViewById(R.id.jpListView);
+                final ArrayAdapter<JourneyRoute> resultAdapter = new ArrayAdapter<JourneyRoute>(getContext(), android.R.layout.simple_list_item_1, android.R.id.text1, jpResult.getRoutes());
+                resultList.setAdapter(resultAdapter);
+                resultList.setOnItemClickListener((parent, view, position, id) -> {
+
+                });
+
+            }, error -> {});
         }
+
+
+
     }
 
     public void switchValues() {
@@ -138,6 +146,76 @@ public class JourneyPlanner implements Behaviour {
 
     public boolean checkToText(){
         return getEditTextContents(R.id.jpTo).isEmpty();
+    }
+
+    public void fromSearch(){
+
+        final EditText fromInput = parentView.findViewById(R.id.jpFrom);
+        String fromInputText = fromInput.getText().toString();
+        final ListView fromDisplayedBusList = parentView.findViewById(R.id.jpListView);
+        final StopByName fromStop = new StopByName(getContext(), fromInputText);
+        fromInput.requestFocus();
+
+        fromStop.query(
+                response -> {
+                    ArrayList<NaptanBusStop> fromBusList = (ArrayList<NaptanBusStop>) response.getMessage().getData();
+                    ArrayList<String> fromBusListString = new ArrayList<>();
+
+                    for (int i = 0; i != fromBusList.size(); i++){
+                        fromBusListString.add(fromBusList.get(i).getName());
+                    }
+
+                    final ArrayAdapter<String> fromBusAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, android.R.id.text1, fromBusListString);
+                    fromDisplayedBusList.setAdapter(fromBusAdapter);
+
+                    fromDisplayedBusList.setOnItemClickListener((parent, view, position, id) -> {
+                        //saving long and lat of selected bus stop.
+                        String fromLonglatSelected = fromBusList.get(position).getLocation().getLongLat();
+                        //bus stop name
+                        String fromBusStopName = (String) fromBusList.get(position).getName();
+                        busStopFrom = fromBusList.get(position);
+                        fromInput.setText(fromBusStopName);
+                        fromBusAdapter.clear();
+
+
+
+                    });
+
+
+                },
+                error -> { }
+        );
+        fromSearchCheck = true;
+    }
+
+    public void toSearch(){
+        final EditText toInput = parentView.findViewById(R.id.jpTo);
+        String toInputText =  toInput.getText().toString();
+        final StopByName toStop = new StopByName(getContext(), toInputText);
+        final ListView toDisplayedBusList = parentView.findViewById(R.id.jpListView);
+
+        toStop.query(
+                response -> {
+                    ArrayList<NaptanBusStop> toBusList = (ArrayList<NaptanBusStop>) response.getMessage().getData();
+                    ArrayList<String> toBusListString = new ArrayList<>();
+                    for (int i = 0; i != toBusList.size(); i++){
+                        toBusListString.add(toBusList.get(i).getName());
+                    }
+
+                    final ArrayAdapter<String> toBusAdapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1, android.R.id.text1, toBusListString);
+                    toDisplayedBusList.setAdapter(toBusAdapter);
+                    toDisplayedBusList.setOnItemClickListener((parent, view, position, id) -> {
+                        String toLonglatSelected = toBusList.get(position).getLocation().getLongLat();
+                        String toBusStopName = (String) toBusList.get(position).getName();
+                        busStopTo = toBusList.get(position);
+                        toInput.setText(toBusStopName);
+                        toBusAdapter.clear();
+                    });
+                    toSearchCheck = true;
+
+                },
+                error -> { }
+        );
     }
 
 
